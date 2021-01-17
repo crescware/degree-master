34333669f0a5f2bc87d695f75cb93c11404ea1ad
OKUNOKENTARO <okunokentaro@me.com>
2021-01-18 00:01:09 +0900

    12 equal temperament

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,19 +2,41 @@ import { Component, OnInit } from '@angular/core';
 
 import { Synth } from './synth';
 
+const note = [
+  'c',
+  'c#',
+  'd',
+  'd#',
+  'e',
+  'f',
+  'f#',
+  'g',
+  'g#',
+  'a',
+  'a#',
+  'b',
+] as const;
+type Note = typeof note[number];
+
+const freqArr = [...Array(24)]
+  .reduce((acc, _, i) => {
+    return acc.concat(220 * 2 ** (i / 12));
+  }, [] as number[])
+  .slice(3, 3 + 13);
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   synth: Synth | null = null;
 
-  ngOnInit() {
+  onClickStart() {
     this.synth = new Synth();
   }
 
-  onMousedown() {
-    this.synth?.play(440, 300);
+  onMousedown(n: Note, oct: 0 | 1) {
+    this.synth?.play(freqArr[note.indexOf(n) + oct * 12], 300);
   }
 }
